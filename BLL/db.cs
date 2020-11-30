6949900ddff4bab3912fc521e9bdb5af6b960c45
Ronny Mata <rmata@itcoint.com>
2020-11-29 23:16:10 -0600

    commit

---
+++ BLL/db.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class db
+    {
+        public SqlConnection conexion = new SqlConnection();
+
+        public SqlConnection ObtenerConexion()
+        {
+            conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL_AUT"].ToString());
+
+
+            try
+            {
+                if (conexion != null && conexion.State == ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+                return conexion;
+            }
+
+            catch (Exception ex)
+            {
+                conexion.Close();
+                throw ex;
+            }
+        }
+
+        public bool DescargarConexion()
+        {
+            conexion.Close();
+            return true;
+        }
+    }
+}
---
+++ Proyecto/Content/js/estudiante.js
@@ -0,0 +1,38 @@
+﻿ 
+$(document).ready(function () {
+             reloadEstudiantes();
+    });
+
+
+function reloadEstudiantes() {
+
+    $.ajax({
+        url: 'Principal.aspx/fillTable',
+        type: "POST",
+        contentType: false,
+        processData: false,
+        async: false,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (datos) {
+
+            if (datos) {
+                var resultAsJson = datos.d // your return result is JS array
+                 //botones = botones + "<button type = 'button' class= 'btn btn-primary  px-3' id = '" + producto.ID + "' data - toggle='modal' data - target='#editarProyectos' onClick = 'editarPRODUCTO(this.id)'><i class='fab fa fa-pencil-square-o' aria-hidden='true'></i></button>";
+                    //botones = botones + "<button type='button' class='btn btn-danger px-3' id='" + producto.ID + "' onClick='deleteProducto(this.id)' ><i class='fas fa fa-trash-o' aria-hidden='true'></i></button>";
+
+                for (var i in resultAsJson) {
+                    var user = resultAsJson[i]
+                    $("#estudianteTabla tbody").append("<tr role='row' class='odd'><td>" + user.id + "</td><td>" + user.nombre + "</td><td>"
+                        + user.apellidos + "</td><td>" + user.edad + "</td><td>" + user.direccion + "</td> ");
+                }
+                
+ 
+
+            }
+
+        },
+        error: function (xhr, ajaxOptions, thrownError) { }
+
+    });
+}
